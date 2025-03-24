ad9e5c3b01bc7cc29e28cf0b29a36760cf940ff1
Naveed Khosa <naveedullahkhosa@gmail.com>
2025-03-24 15:05:16 +0500

    Hero changes

--- user-site/src/pages/UserDashbord/PlanManage.jsx
+++ user-site/src/pages/UserDashbord/PlanManage.jsx
@@ -76,24 +76,14 @@ const PlanManage = () => {
             <div className="flex justify-center items-center bg-gray-100 rounded-full p-1.5 max-w-sm mx-auto">
               <button
                 onClick={() => setActiveTab("yearly")}
-                className={`w-1/2 text-center rounded-full py-3 px-3 lg:px-11 font-semibold transition-all duration-500 ${activeTab === "yearly" ? " text-white" : "text-gray-400 hover:text-indigo-600"}`}
-                style={{
-                  background: "linear-gradient(to right, #3b82f6, #9333ea)", 
-                  background: "-webkit-linear-gradient(left, #3b82f6, #9333ea)", 
-                  background: "-moz-linear-gradient(left, #3b82f6, #9333ea)", 
-                  background: "-o-linear-gradient(left, #3b82f6, #9333ea)"
-                }} >
+                className={`w-1/2 text-center rounded-full py-3 px-3 lg:px-11 font-semibold transition-all duration-500 ${activeTab === "yearly" ? "bg-gradient-to-r from-blue-500 to-purple-500 text-white" : "text-gray-400 hover:text-indigo-600"}`}
+               >
                 Bill Yearly
               </button>
               <button
                 onClick={() => setActiveTab("monthly")}
-                className={`w-1/2 text-center rounded-full py-3 px-3 lg:px-11 font-semibold transition-all duration-500 ${activeTab === "monthly" ? " text-white" : "text-gray-400 hover:text-indigo-600"}`}
-                style={{
-                  background: "linear-gradient(to right, #3b82f6, #9333ea)", 
-                  background: "-webkit-linear-gradient(left, #3b82f6, #9333ea)", 
-                  background: "-moz-linear-gradient(left, #3b82f6, #9333ea)", 
-                  background: "-o-linear-gradient(left, #3b82f6, #9333ea)"
-                }} >
+                className={`w-1/2 text-center rounded-full py-3 px-3 lg:px-11 font-semibold transition-all duration-500 ${activeTab === "monthly" ? " bg-gradient-to-r from-blue-500 to-purple-500 text-white" : "text-gray-400 hover:text-indigo-600"}`}
+              >
                 Bill Monthly
               </button>
             </div>
@@ -113,13 +103,8 @@ const PlanManage = () => {
                 {plans.map((plan) => (
                   <div
                     key={plan.id}
-                    className="group relative flex flex-col mx-auto w-full max-w-sm rounded-2xl p-6 xl:p-12 transition-all duration-300  text-white hover:bg-indigo-700"
-                    style={{
-                      background: "linear-gradient(to right, #3b82f6, #9333ea)", 
-                      background: "-webkit-linear-gradient(left, #3b82f6, #9333ea)", 
-                      background: "-moz-linear-gradient(left, #3b82f6, #9333ea)", 
-                      background: "-o-linear-gradient(left, #3b82f6, #9333ea)"
-                    }}   >
+                    className="group relative flex flex-col mx-auto w-full max-w-sm rounded-2xl p-6 xl:p-12 transition-all duration-300 bg-gradient-to-r from-blue-500 to-purple-500 text-white hover:bg-indigo-700"
+                    >
                     <h3 className="font-manrope text-2xl font-bold mb-6">{plan.plan_name}</h3>
                     <ul className="space-y-4 text-left mb-6">
                       {plan.PlanFeatures.split("|").map((feature, index) => (
